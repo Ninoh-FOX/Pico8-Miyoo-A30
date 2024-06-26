d7bffe363b36447066563a4bb8648aee2e7b0e7a
Ninoh-FOX <ninohfox@gmail.com>
2024-06-26 13:08:05 +0200

    chaged load video type in fullscreen mode, initial mouse support

--- Emu/PICO8/launch3.sh
+++ Emu/PICO8/launch3.sh
@@ -4,14 +4,14 @@ export HOME="$picodir"
 
 export PATH="$HOME"/bin:$PATH
 export LD_LIBRARY_PATH="$HOME"/lib:$LD_LIBRARY_PATH
-export SDL_VIDEODRIVER=a30
+export SDL_VIDEODRIVER=mali
 export SDL_JOYSTICKDRIVER=a30
 
 cd "$picodir"
 
-sed -i 's|^transform_screen 135$|transform_screen 0|' "$HOME/.lexaloffle/pico-8/config.txt"
+sed -i 's|^transform_screen 0$|transform_screen 135|' "$HOME/.lexaloffle/pico-8/config.txt"
 
 cpuclock performance 1 1000 384
 
-pico8_dyn -width 640 -height 480 -scancodes -run "$1"
+pico8_dyn -width 640 -height 480 -draw_rect 0,0,480,640 -run "$1"
 sync
